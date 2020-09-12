1fa3d20be5e463d54b790f68d024f2d063051925
Claire Rogers <claire@guiltydog.co.uk>
2020-09-12 17:32:08 +0100

    Chapter 4 Gaze based control.

---
+++ Assets/LookMoveTo.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookMoveTo : MonoBehaviour
+{
+    public GameObject ground;
+    private Transform camera;
+    private int layerMask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = Camera.main.transform;
+        layerMask = 1 << 8;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray;
+//        RaycastHit hit;
+        RaycastHit[] hits;
+        GameObject hitObject;
+
+        Debug.DrawRay(camera.position, camera.rotation * Vector3.forward * 100.0f);
+
+        ray = new Ray(camera.position, camera.rotation * Vector3.forward);
+        //        Physics.Raycast(ray, out hit, 20.0f, layerMask);
+        hits = Physics.RaycastAll(ray);
+        for (int i = 0; i < hits.Length; i++)
+        {
+            RaycastHit hit = hits[i];
+            hitObject = hit.collider.gameObject;
+            if (hitObject == ground)
+            {
+                Debug.Log("Hit (x, y, z): " + hit.point.ToString("F2"));
+                transform.position = hit.point;
+            }
+        }
+    }
+}
