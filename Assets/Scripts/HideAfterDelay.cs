6418306a54897882b10c5eb6888a43670ae36128
Claire Rogers <claire@guiltydog.co.uk>
2020-09-17 19:35:04 +0100

    Hide UI canvas after delay.

---
+++ Assets/Scripts/HideAfterDelay.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideAfterDelay : MonoBehaviour
+{
+    public float delayInSeconds = 5.0f;
+    public float fadeRate = 0.25f;
+
+    private CanvasGroup canvasGroup;
+    private float startTimer;
+    private float fadeoutTimer;
+
+    void OnEnable()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+        canvasGroup.alpha = 1.0f;
+
+        startTimer = Time.time + delayInSeconds;
+        fadeoutTimer = fadeRate;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= startTimer)
+        {
+            fadeoutTimer -= Time.deltaTime;
+
+            if (fadeoutTimer <= 0)
+            {
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                canvasGroup.alpha = fadeoutTimer / fadeRate;
+            }
+        }
+    }
+}
