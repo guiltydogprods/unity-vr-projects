e36beb9016fba1083714ec7c3947f5c1c43f1cfd
Claire Rogers <claire@guiltydog.co.uk>
2020-09-25 20:35:27 +0100

    Direct interaction and palette UI.

---
+++ Assets/ForceToggle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ForceToggle : MonoBehaviour
+{
+    private Toggle toggle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        toggle = GetComponent<Toggle>();   
+    }
+
+    // Update is called once per frame
+    public void Toggle()
+    {
+        toggle.isOn = !toggle.isOn;
+    }
+}
