72fe09236099c30ff84d48a1d7dbd3ec53718aba
Claire Rogers <claire@guiltydog.co.uk>
2020-09-29 09:01:59 +0100

    HandleScript.cs

---
+++ Assets/Scripts/HandleScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+
+public class HandleScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
