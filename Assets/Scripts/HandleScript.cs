cf1a516002ede424e07335e09b9b4eb28e7d6664
Claire Rogers <claire@guiltydog.co.uk>
2020-10-02 19:02:06 +0100

    Simply UI for DoorTest.

--- Assets/Scripts/HandleScript.cs
+++ Assets/Scripts/HandleScript.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.UI;
 
 
 public class HandleScript : MonoBehaviour
 {
     public Transform handle;
 
+    [SerializeField] private Text text = default;
     private XRSimpleInteractable interactable;
     private Rigidbody rigidbody;
     private bool isGrabbing;
+    private bool applyFix;
     private Vector3 handPosition;
 
     // Start is called before the first frame update
@@ -19,6 +22,8 @@ public class HandleScript : MonoBehaviour
         interactable = GetComponent<XRSimpleInteractable>();
         rigidbody = GetComponent<Rigidbody>();
         isGrabbing = false;
+        applyFix = false;
+        text.text = "Fix not applied";
     }
 
     public void Grab()
@@ -53,6 +58,26 @@ public class HandleScript : MonoBehaviour
         {
             handPosition = InteractorPosition();
             transform.position = handPosition;
+            //            Debug.DrawLine(transform.position, CurrentInteractor.transform.position, Color.white);
+//            Debug.DrawLine(transform.position, transform.position + transform.forward * 0.05f, Color.red);
+//            Debug.DrawLine(transform.position, transform.position + transform.up * 0.05f, Color.green);
+//            Debug.DrawLine(transform.position, transform.position + transform.right * 0.05f, Color.blue);
+
+        }
+        if (Input.GetButtonDown("XRI_Left_PrimaryButton"))
+        {
+            applyFix ^= true;
+            if (applyFix == true)
+            {
+                text.text = "Fix applied";
+            }
+            else
+            {
+                text.text = "Fix not applied";
+            }
+        }
+        else if (Input.GetButtonUp("XRI_Left_PrimaryButton"))
+        {
         }
     }
 
@@ -62,10 +87,13 @@ public class HandleScript : MonoBehaviour
 // CLR - Comment out these two lines to remove the Grabbable Handle position reset on release.  It feels nicer when letting go of the handle but doesn't reset the 
 //       grabbable handle position.  With these two lines in, the position is reset, but letting go of the door handle causes the door to move in a bad way. I'm sure
 //       it's fixable but requires a bit more work to figure out what is going on.
-        transform.position = handle.transform.position;
-        transform.rotation = handle.transform.rotation;
-//        rigidbody.velocity = Vector3.zero;
-//        rigidbody.angularVelocity = Vector3.zero;
-//        rigidbody.isKinematic = false;
+        if (applyFix)
+        {
+            transform.position = handle.transform.position;
+            transform.rotation = handle.transform.rotation;
+        }
+        //        rigidbody.velocity = Vector3.zero;
+        //        rigidbody.angularVelocity = Vector3.zero;
+        //        rigidbody.isKinematic = false;
     }
 }
